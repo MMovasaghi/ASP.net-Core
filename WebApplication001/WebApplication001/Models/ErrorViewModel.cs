1dab7c3932c51722dc2b5654d2dfd08542c47a46
MH.Movasaghi <mmghho77@gmail.com>
2019-02-27 17:05:37 +0330

    Initial Project WebApplication001

---
+++ WebApplication001/WebApplication001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
