5a4d2eee27e5c30c875dd53442c8185767313577
MH.Movasaghi <mmghho77@gmail.com>
2019-03-21 18:14:05 +0330

    Finish CodeFirstDB

--- CodeFirstDB/CodeFirstDB/Program.cs
+++ CodeFirstDB/CodeFirstDB/Program.cs
@@ -10,6 +10,19 @@ namespace CodeFirstDB
     {
         static void Main(string[] args)
         {
+            using (var db = new PersonDB())
+            {
+                db.Blogs.Add(new Person { Name = "Ali" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var person in db.Blogs)
+                {
+                    Console.WriteLine("{0} - {1}", person.Id, person.Name);
+                }
+            }
         }
     }
 }
