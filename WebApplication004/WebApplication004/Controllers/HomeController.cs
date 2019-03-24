dfdcba6851565b206f847e47b4850952eae85845
MH.Movasaghi <mmghho77@gmail.com>
2019-03-24 08:12:08 +0430

    Changes has been applied #T01
    if the changes is in the c# classes , if we run the app as the "Start app without debugging" the changes has been applied , but if we use "Start with debugging" the changes has not been applied so you must be restart the app

--- WebApplication004/WebApplication004/Controllers/HomeController.cs
+++ WebApplication004/WebApplication004/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication004.Models;
+using System.Linq;
 
 namespace WebApplication004.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where(p => p.Price < 40));
     }
 }
