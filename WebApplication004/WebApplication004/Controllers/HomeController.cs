dd5d2cc41312c07cae57baa1f0fbc705e3b7e2bb
MH.Movasaghi <mmghho77@gmail.com>
2019-03-24 18:09:58 +0430

    Add index.cshtml bootstrap

--- WebApplication004/WebApplication004/Controllers/HomeController.cs
+++ WebApplication004/WebApplication004/Controllers/HomeController.cs
@@ -6,6 +6,17 @@ namespace WebApplication004.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 40));
+        SimpleRepository Repository = SimpleRepository.SharedRepository;
+        public IActionResult Index() => View(Repository.Products.Where(p => p?.Price < 50));
+
+        [HttpGet]
+        public IActionResult AddProduct() => View(new Product());
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            Repository.AddProduct(p);
+            return RedirectToAction("Index");
+        }
     }
 }
