a449d838d4e0e122336d692b3c19098416e8a3f7
MH.Movasaghi <mmghho77@gmail.com>
2019-03-24 07:36:31 +0430

    Add HomeController to Controllers

---
+++ WebApplication004/WebApplication004/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication004.Models;
+
+namespace WebApplication004.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+    }
+}
