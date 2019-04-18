ba1c5c2f8f1224096315a822e6edb2cdd9ac4d3c
MH.Movasaghi <mmghho77@gmail.com>
2019-04-18 15:29:29 +0430

    Init Unit Test

---
+++ SportStoreSimpleCode/SportStoreSimpleCode.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SportStoreSimpleCode.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
