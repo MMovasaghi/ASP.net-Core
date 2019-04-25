63872b76c294a78acd79d87e334fac8f0527ff41
MH.Movasaghi <mmghho77@gmail.com>
2019-04-25 15:30:16 +0430

    adding unit test

--- SportStoreSimpleCode/SportStoreSimpleCode/Controllers/ProductController.cs
+++ SportStoreSimpleCode/SportStoreSimpleCode/Controllers/ProductController.cs
@@ -15,10 +15,6 @@ namespace SportStoreSimpleCode.Controllers
         {
             repository = repo;
         }
-        public ViewResult List(int productPage = 1)
-            => View(repository.Products
-            .OrderBy(p => p.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize));
+        public ViewResult List(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
     }
 }
--- SportStoreSimpleCode/XUnitTestProject1/ProductControllerTests.cs
+++ SportStoreSimpleCode/XUnitTestProject1/ProductControllerTests.cs
@@ -1,9 +1,11 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using SportStoreSimpleCode;
+using System.Linq;
+using SportStoreSimpleCode.Controllers;
+using SportStoreSimpleCode.Models;
+using Xunit;
+using Moq;
 
-namespace SportStoreSimpleCode.Test
+namespace XUnitTestProject1
 {
     public class ProductControllerTests
     {
@@ -12,20 +14,17 @@ namespace SportStoreSimpleCode.Test
         {
             // Arrange
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns((new Product[] 
-            {
-                 new Product {ProductID = 1, Name = "P1"},
-                 new Product {ProductID = 2, Name = "P2"},
-                 new Product {ProductID = 3, Name = "P3"},
-                 new Product {ProductID = 4, Name = "P4"},
-                 new Product {ProductID = 5, Name = "P5"}
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
             }).AsQueryable<Product>());
-
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            IEnumerable<Product> result =
-            controller.List(2).ViewData.Model as IEnumerable<Product>;
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
             // Assert
             Product[] prodArray = result.ToArray();
             Assert.True(prodArray.Length == 2);
