50972a309b4a11bd9cefb9ab8f38ba632c01fed2
MH.Movasaghi <mmghho77@gmail.com>
2019-08-16 18:40:58 +0430

    Add Part-001/P001
    The First Project with Asp.net Core

---
+++ Part-001/P001/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace P001
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            string result = "Hi, It's me : Mohammad Hossein Movasaghinia \nIt's the first project , So Hello World ! :)";
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(result);
+            });
+
+            
+        }
+    }
+}
