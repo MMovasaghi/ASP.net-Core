9ed54ae9d52ef21c6df7eb7dee882674aef484a6
mmghho77@gmail.com <ASUS@DESKTOP-FD8B3TF>
2020-03-07 18:28:41 +0330

    add user edit profile part 2 (backend and ui fixed)

--- app003/Controllers/UsersController.cs
+++ app003/Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using app003.Data;
 using app003.Dtos;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace app003.Controllers
 {
@@ -36,6 +38,20 @@ namespace app003.Controllers
             var user = await _repo.GetUser(id);
             var userToReturn  = _mapper.Map<UserForDetailedDto>(user);
             return Ok(userToReturn);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save.");
         }
     }
 }
--- app004/src/app/members/member-edit/member-edit.component.ts
+++ app004/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -20,7 +22,7 @@ export class MemberEditComponent implements OnInit {
   }
   user: User;
   
-  constructor(private route: ActivatedRoute, private alertiry: AlertifyService,) { }
+  constructor(private route: ActivatedRoute, private alertiry: AlertifyService, private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -29,9 +31,12 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertiry.success("Profile updated successfully.");
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertiry.success("Profile updated successfully.");
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertiry.error(error);
+    });    
   }
 
 }
